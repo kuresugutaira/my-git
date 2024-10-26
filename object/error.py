5dc5004d3e942304d936f49f6c19944808878ede
kuresugutaira <kuresugutaira@gmail.com>
2024-10-26 11:49:10 +0900

    Exceptionクラスを記述するerror.pyを追加

---
+++ object/error.py
@@ -0,0 +1,17 @@
+class InValidObjectException():
+  def __str__(self):
+    return (
+      f"invalid object"
+    )
+  
+class NotCommitException():
+  def __str__(self):
+    return (
+      f"not commit object"
+    )
+  
+class InvalidCommitObject():
+  def __str__(self):
+    return (
+      f"invalid commit object"
+    )
